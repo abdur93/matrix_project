12b4af031285ecba83c3dc214957d4ce17c73258
Abdur rahman <abdurrahman01993@gmail.com>
2023-01-19 18:05:04 +0600

    A project seemore function implement

---
+++ js/abdur.js
@@ -0,0 +1,90 @@
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+function myGlobalFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("tore");
+    var btnText = document.getElementById("myButton");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+ 
+          
+  // natural organic Biological safe
+
+
+        // natural function
+  function naturalFunction() {
+    var dots = document.getElementById("dott");
+    var moreText = document.getElementById("readen");
+    var btnText = document.getElementById("btnSubmit");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+      // organic function
+
+  function organicFunction() {
+    var dots = document.getElementById("koma");
+    var moreText = document.getElementById("besi");
+    var btnText = document.getElementById("btnS");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+          // biological safe
+  function biologicalFunction() {
+    var dots = document.getElementById("bunda");
+    var moreText = document.getElementById("much");
+    var btnText = document.getElementById("btnO");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
